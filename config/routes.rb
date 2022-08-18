f2205086ad3b58c022ccdf1b16111edffc58cd20
karen2093 <108413518+karen2093@users.noreply.github.com>
2022-08-18 10:46:12 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :suscriptors
+  get 'home/about'
+  get 'home/nacional'
+  get 'home/politica'
+  get 'home/internacional'
+  get 'home/deportes'
+  get 'home/farandula'
+  get 'home/contacto'
+  root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
